bdc4013968bcb3392ebd1b80a517e683fc1cc813
Akiya <kd1220691@st.kobedenshi.ac.jp>
2024-04-29 16:15:16 +0900

    WrapperOpensslとOpenSocketの連携ようにCMakeLists.txtを修正

--- src/Library/OpenSocket/OpenSocket_Def.h
+++ src/Library/OpenSocket/OpenSocket_Def.h
@@ -5,7 +5,6 @@ constexpr int IPV4 = 1;
 constexpr int IPV6 = 2;
 constexpr int IPVD = 3;
 constexpr long SEQUENCEMAX = 4200000000;
-// constexpr int TCP_BASE_HEADER_SIZE = sizeof(int) + sizeof(char) + sizeof(char) + sizeof(short);
 constexpr int TCP_BASE_HEADER_SIZE = sizeof(int);
 
 constexpr int RECV_PACKET_MAX_SIZE = 4194304;
@@ -17,20 +16,20 @@ constexpr int BODY_MAX_SIZE = 2048;
 
 // MSとそれ以外のOSで使用する型の統一用マクロ
 #ifdef _MSC_VER
-#define B_SOCKET SOCKET
-#define B_INIT_SOCKET INVALID_SOCKET
-#define B_ADDRESS sockaddr
-#define B_ADDRESS_LEN int
-#define B_ADDRESS_IN sockaddr_in
-#define B_SHUTDOWN SD_BOTH
+   #define B_SOCKET SOCKET
+   #define B_INIT_SOCKET INVALID_SOCKET
+   #define B_ADDRESS sockaddr
+   #define B_ADDRESS_LEN int
+   #define B_ADDRESS_IN sockaddr_in
+   #define B_SHUTDOWN SD_BOTH
 
 #else
-#define B_SOCKET int
-#define B_INIT_SOCKET -1
-#define B_ADDRESS struct sockaddr
-#define B_ADDRESS_LEN socklen_t
-#define B_ADDRESS_IN sockaddr_in
-#define B_SHUTDOWN SHUT_RDWR
+   #define B_SOCKET int
+   #define B_INIT_SOCKET -1
+   #define B_ADDRESS struct sockaddr
+   #define B_ADDRESS_LEN socklen_t
+   #define B_ADDRESS_IN sockaddr_in
+   #define B_SHUTDOWN SHUT_RDWR
 #endif
 
 }  // namespace OpenSocket
