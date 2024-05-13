22fdc5c6d971ffcc7c4748315d0f78bf29a8c961
Akiya <kd1220691@st.kobedenshi.ac.jp>
2024-05-13 23:42:45 +0900

    TCP_Cipher_Client.hのタイポ修正

--- src/Library/OpenSocket/TCP_Cipher_Client.h
+++ src/Library/OpenSocket/TCP_Cipher_Client.h
@@ -11,7 +11,7 @@ public:
    // UpdateのoverrideからDataProcessingのoverrideに修正する。
    virtual void Update() override;
    int CiphserSendServer(const char* _buf, const int _bufSize, const char _firstClass, const char _secondClass, const char _firstOption, const char _secondOption);  // 通信用の送信処理
-   bool KeyChangeConnectionStart(int _rsaKeyByteSize, int _aesKeyBitSize);
+   bool KeyChangeConnectionStart(int _rsaKeyByteSize, int _aesKeyByteSize);
 
 private:
    void DataProcessing();
